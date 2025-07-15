73482379cc77058f8090e1f922139e9e94e5c80c
shuvo <isuvo@outlook.com>
2025-07-15 03:31:52 -0400

    Update favicon path in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ProjectConference/', 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
